2e9714f4c3e8029cdfe0956e659c8994851727f4
= <stankokuveljic@gmail.com>
2017-02-12 20:02:29 +0100

    Change strucure of config file

--- src/main/java/util/Conf.java
+++ src/main/java/util/Conf.java
@@ -2,6 +2,7 @@ package util;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.jena.base.Sys;
 
 /**
  * This will be bean or something like that
@@ -10,6 +11,7 @@ public class Conf {
     private static Conf instance = null;
     private Config conf;
     private String pdfRoot;
+    private String storageRoot;
 
 
     private Conf(){
@@ -18,7 +20,8 @@ public class Conf {
 
     private void initialize(){
         this.conf = ConfigFactory.load("application.conf");
-        this.pdfRoot = conf.getString("pdf_root");
+        this.storageRoot = conf.getConfig("storage").getString("store_root");
+        this.pdfRoot = storageRoot +  conf.getConfig("storage").getConfig("pdf").getString("root");
     }
 
     public static Conf getInstance(){
@@ -32,9 +35,14 @@ public class Conf {
         return this.pdfRoot;
     }
 
+    public String getStorageRoot(){
+        return this.storageRoot;
+    }
+
     public static void main(String[] args){
         Conf c = Conf.getInstance();
 
         System.out.println(c.getPdfRoot());
+        System.out.println(c.getStorageRoot());
     }
 }
